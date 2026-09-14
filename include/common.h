2cf64ef6ed65aafac501543497c896fa76a2a90e
Rocco Himel <rockyrbn1847@gmail.com>
2026-09-13 21:03:07 -0400

    'Modulon 1.1-beta5' -> 'Modulon 1.1-rc1'

--- include/common.h
+++ include/common.h
@@ -23,7 +23,7 @@
 #define PATH_MAX 4096
 #endif
 
-#define VERSION "Modulon 1.1-beta5"
+#define VERSION "Modulon 1.1-rc1"
 #define ARR_GROW(a, n, cap, type)                                  \
 	do                                                         \
 	{                                                          \
